1434ce3c80e32c1d35748d951580424de459ae9a
theiereman <thomas.eyermann@protonmail.com>
2026-06-22 20:00:11 +0200

    feat: better activities progress display

--- app/presenters/progress_presenter.rb
+++ app/presenters/progress_presenter.rb
@@ -1,7 +1,7 @@
 class ProgressPresenter
   delegate_missing_to :@progress
 
-  Slot = Data.define(:hour, :activity) do
+  Slot = Data.define(:hour, :activity, :night) do
     def filled? = activity.present?
     def label = filled? ? activity.category.label : "Non renseigné"
   end
@@ -13,8 +13,10 @@ class ProgressPresenter
   def slots
     activities_by_hour = @progress.activities.index_by { |activity| activity.started_at.hour }
 
-    (schedule.wake_up_hour...schedule.sleep_hour).map do |hour|
-      Slot.new(hour: hour, activity: activities_by_hour[hour])
+    (0..23).map do |hour|
+      Slot.new(hour: hour, activity: activities_by_hour[hour], night: !hour.between?(schedule.wake_up_hour, schedule.sleep_hour))
     end
   end
+
+  private
 end
