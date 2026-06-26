6b3c8b444546b1b4a40d36a45c88fdc2c2616df4
theiereman <thomas.eyermann@protonmail.com>
2026-06-26 14:48:53 +0200

    ci: updated mailer default from

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "Time Tracker <no-reply@time-tracker.local>"
+  default from: "Time Tracker <contact@dotsncircles.com>"
   layout "mailer"
 end
