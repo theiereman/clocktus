ae3393fe7c39de22986f94ded018347d6ab22748
theiereman <thomas.eyermann@protonmail.com>
2026-07-03 11:18:06 +0200

    fix: year_select_values helper fix when no activities

--- app/helpers/statistics_helper.rb
+++ app/helpers/statistics_helper.rb
@@ -1,10 +1,13 @@
 module StatisticsHelper
   def year_select_values
+    res = []
+    res << [ t("statistics.show.filters.all_years"), nil ]
+
     first_activity = Current.user&.activities&.order(started_at: :asc)&.first
     last_activity = Current.user&.activities&.order(started_at: :desc)&.first
 
-    res = []
-    res << [ t("statistics.show.filters.all_years"), nil ]
+    return res unless first_activity.present? && last_activity.present?
+
     res + (first_activity.started_at.year..last_activity.started_at.year).to_a.map do |year|
       [ year, year ]
     end
