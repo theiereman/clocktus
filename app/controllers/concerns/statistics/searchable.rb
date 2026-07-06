a83551f6d1afe59c81977b1bfd7a047b7cf4646a
theiereman <thomas.eyermann@protonmail.com>
2026-07-06 15:10:50 +0200

    feat: removed the current day from the stats as its not complete and will cause graph crashes

--- app/controllers/concerns/statistics/searchable.rb
+++ app/controllers/concerns/statistics/searchable.rb
@@ -2,7 +2,7 @@ module Statistics::Searchable
   extend ActiveSupport::Concern
 
   def filtered_activities(user)
-    @activities = user.activities
+    @activities = user.activities.where("started_at < ?", Date.current)
     @activities = @activities.year(params[:year]) if params[:year].present?
     @activities
   end
