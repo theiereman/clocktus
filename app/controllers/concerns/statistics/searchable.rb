5c7a4340647d1624ed631de5d269940d8d1fe767
theiereman <thomas.eyermann@protonmail.com>
2026-07-29 12:08:51 +0200

    feat: added shortcuts to filters

--- app/controllers/concerns/statistics/searchable.rb
+++ app/controllers/concerns/statistics/searchable.rb
@@ -1,27 +1,28 @@
 module Statistics::Searchable
   extend ActiveSupport::Concern
 
-  def filtered_activities(user)
-    set_filter_values(user)
-
-    progress = User::Progress.between(user, @from, @to)
-    activities = progress.activities
-    return activities unless @full_days_only
-
-    complete_days = activities
-      .map { |activity| activity.started_at.to_date }
-      .uniq
-      .select { |day| progress.all_activities_done?(date: day) }
-      .to_set
-
-    activities.select { |activity| complete_days.include?(activity.started_at.to_date) }
+  included do
+    helper_method :filtered_on_today?
+    helper_method :filtered_on_month?
+    helper_method :filtered_on_year?
   end
 
-  private
-
-  def set_filter_values(user)
+  def filtered_activities(user)
     @from = params[:from]&.to_date || user.activities.minimum(:started_at)&.to_date || Date.current
     @to = params[:to]&.to_date || user.activities.maximum(:started_at)&.to_date || Date.current
-    @full_days_only = ActiveModel::Type::Boolean.new.cast(params[:full_days_only])
+
+    progress = User::Progress.between(user, @from, @to).activities
+  end
+
+  def filtered_on_today?
+    @from == Date.current && @to == Date.current
+  end
+
+  def filtered_on_month?
+    @from == Date.current.beginning_of_month && @to == Date.current.end_of_month
+  end
+
+  def filtered_on_year?
+    @from == Date.current.beginning_of_year && @to == Date.current.end_of_year
   end
 end
