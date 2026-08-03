626fa11b08ab3096d31ae20987f8b9233dd211f9
theiereman <thomas.eyermann@protonmail.com>
2026-08-03 15:27:41 +0200

    feat: added week filter

--- app/controllers/concerns/statistics/searchable.rb
+++ app/controllers/concerns/statistics/searchable.rb
@@ -4,6 +4,7 @@ module Statistics::Searchable
   included do
     helper_method :single_day?
     helper_method :filtered_on_today?
+    helper_method :filtered_on_week?
     helper_method :filtered_on_month?
     helper_method :filtered_on_year?
   end
@@ -21,6 +22,11 @@ module Statistics::Searchable
 
   def filtered_on_today?
     @from == Date.current && @to == Date.current
+  end
+
+
+  def filtered_on_week?
+    @from == Date.current.beginning_of_week && @to == Date.current.end_of_week
   end
 
   def filtered_on_month?
